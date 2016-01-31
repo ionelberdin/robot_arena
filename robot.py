4ea752cdbbb4477f02959b416be21cae4a50d1df
Ion <ionelberdin@gmail.com>
2016-01-31 08:48:22 +0100

    reconfigure robot definitio

--- robot.py
+++ robot.py
@@ -4,24 +4,44 @@ import pygame
 
 
 class Robot(pygame.sprite.Sprite):
-    size = width, height = 30, 30
 
-    def __init__(self, pos, color):
+    def __init__(self, position, angle, goal, color, width, choose_move):
         pygame.sprite.Sprite.__init__(self)
-        self.pos = np.array(pos)
+
+        self.position = np.array(position)
+        self.angle = angle
+        self.goal = goal
         self.color = color
+        self.width = width
+        self.choose_move = choose_move
+
+        self.size = width, width
         self.image_0 = pygame.Surface(self.size)
         self.image_0.fill(self.color)
+
+        self.alive = True
         self.speed = 0.
-        self.angle = 0.5
-        self.omega = 0.1
+        self.omega = 0.
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, self.px_pos, self.width/2, 0)
 
     def update(self, dt=0.1):
         self.angle += self.omega * dt
-        self.image = pygame.transform.rotate(pygame.surface.Surface, self.image_0)
+        # self.image = pygame.transform.rotate(pygame.surface.Surface,
+        #                                      self.image_0)
+        self.image = self.image_0
         self.rect = self.image.get_rect()
-        self.pos = self.pos + self.speed * self.vec * dt
+        self.position = self.position + self.speed * self.direction * dt
 
     @property
-    def vec(self):
+    def direction(self):
         return np.array([np.cos(self.angle), np.sin(self.angle)])
+
+    @property
+    def px_pos(self):
+        return tuple([int(pos) for pos in self.position])
+
+    @property
+    def state(self):
+        return self.alive, self.position, self.goal
